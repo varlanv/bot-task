634fa7e19bd825f59b31155dc467b8dd262b6bd8
am <am@localhost.localdomain>
2019-05-31 13:12:39 +0300

    refactor

--- src/main/java/com/task/bottask/CmdRunner.java
+++ src/main/java/com/task/bottask/CmdRunner.java
@@ -18,22 +18,25 @@ public class CmdRunner implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         System.out.println("Supported commands are: " + "\n" +
-                "Who is head of department {department name}" + "\n" +
-                "Show {department name} statistic" + "\n" +
-                "Show the average salary for department {department name}" + "\n" +
-                "Show count of employee for {department name}" + "\n" +
-                "Global search by {template}" + "\n" +
-                "exit");
+                "- Who is head of department {department name}" + "\n" +
+                "- Show {department name} statistic" + "\n" +
+                "- Show the average salary for department {department name}" + "\n" +
+                "- Show count of employee for {department name}" + "\n" +
+                "- Global search by {template}" + "\n" +
+                "- exit" + "\n" +
+                "__________________________________________________________");
 
         Scanner scanner = new Scanner(System.in);
+
         while (true) {
             String line = scanner.nextLine();
 
-
-            try {
-                System.out.println(facade.getAnswer(line));
-            } catch (Exception ex) {
-                System.out.println(ex.getMessage());
+            if (!line.isEmpty()) {
+                try {
+                    System.out.println(facade.getAnswer(line));
+                } catch (Exception ex) {
+                    System.out.println(ex.getMessage());
+                }
             }
         }
     }
